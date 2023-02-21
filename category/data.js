6af3785f45838cd50f29e058a380f5bd5676f61b
BabyGupta05 <codecode355@gmail.com>
2023-02-21 14:57:08 +0530

    category page

---
+++ category/data.js
@@ -0,0 +1,43 @@
+let movies=[
+   
+    {
+        name:"Malikappuram",
+        des:"Little Kallu longs for a glimpse of Swami Ayyappan. When she sets out on the pilgrimage to Sabarimala, a mysterious person steps forward to help. Who is he?",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/9846/1469846-h-a49f352abc84" 
+    },
+    {
+        name:"Black Panther: Wakanda Forever",
+        des:"Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers after the death of beloved King T’Challa.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5119/1455119-h-2a917e92adac"
+    },
+    {
+        name:"Baaghi 3",
+        des:"In a twisted turn of events, when a group of deadly terrorists kidnap Ronnie's elder brother in Syria, he sets off on a mission to bring him back.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/6535/846535-h"
+    },
+    {
+        name:"Govinda Naam Mera",
+        des:"Wedged between a court case, a bad marriage and his girlfriend, Govinda Waghmare gets further entangled in incidents complicating his already tangled world.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4786/1434786-h-cd7bd55f88a6"
+    },
+    {
+        name:"Fursat",
+        des:"An ancient relic lets a man peek into the future. In trying to control what lies ahead, he risks all that matters most in the present. Shot on iPhone 14 Pro",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/3231/1463231-h-c050b2650ddb"
+    },
+    {
+        name:"Drishyam",
+        des:"Story of a common family man who takes desperate measures to save his wife and two daughters when they unwittingly get entangled into a crime.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/old_images/MOVIE/4189/1000074189/1000074189-h"
+    },
+    {
+        name:"The Return of Abhimanyu",
+        des:"When money goes missing from Major Kathiravan's account, resulting in a personal tragedy, he decides to track down the hacker behind this.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5453/305453-h"
+    },
+    {
+        name:"The 100th",
+        des:"After meeting on a dating site, Purab and Malvika plan a special date night. Soon, the casual hook-up blossoms into love as past stories come to the surface.After meeting on a dating site, Purab and Malvika plan a special date night. Soon, the casual hook-up blossoms into love as past stories come to the surface.",
+        image:"https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/9088/1439088-h-541438e09215",
+    }
+]
